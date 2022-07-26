9d0fae14cbeb5ac5b388db01ccbb68874c3828d9
Abhishekkuntare <abhishekkuntare7@gmail.com>
2022-07-27 02:06:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import Sphere from "./components/AnimatedSphere";
+import Iphone from "./components/Iphone";
+import Lambo from "./components/Lambo";
+import My_room from "./components/My_room";
+import Room from "./components/Room";
+import Location from "./components/Location";
+import City from "./components/City";
+import { OrbitControls } from "@react-three/drei";
+import Spider from "./components/Spider";
+import Abhishek from "./components/Abhishek";
+import Joke from "./components/Joke";
+import styled from "styled-components";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Wrapper className="App">
+        <Canvas className="canvas">
+          <OrbitControls />
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[-2, 5, 2]} intensity={0.5} />
+          <Suspense fallback={null}>
+            {/* <Spider /> */}
+            {/* <Joke /> */}
+            <Location />
+            {/* <City /> */}
+            {/* <Lambo /> */}
+            {/* <Iphone /> */}
+            {/* <Room /> */}
+            {/* <Sphere /> */}
+          </Suspense>
+        </Canvas>
+        <Canvas className="canvas">
+          <OrbitControls />
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[-2, 5, 2]} intensity={0.5} />
+          <Suspense fallback={null}>{/* <Joke /> */}</Suspense>
+        </Canvas>
+      </Wrapper>
+    </>
   );
 }
-
-export default App;
+const Wrapper = styled.div`
+  Canvas {
+    background-color: #000;
+    width: 100vw;
+    height: 100vh;
+    margin-left: auto;
+    margin-right: auto;
+  }
+`;
